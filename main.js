f63453294830b6b77cde8956a1307cd4e350b453
tgnlex <alexanderwebber76@gmail.com>
2024-03-22 05:42:06 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import './style.css';
+import $ from 'jquery';
+
+
+
+$('#submit')
+  .on("click",
+    function(){
+      var value = $('#new-todo').val();
+      let todoItem = `
+      <li class="todo-item">
+        <p class="todo-text">
+          ${value}
+        </p>
+        <div class="delete"><b>x</b></div>
+      </li>
+      `
+      console.log(value);
+      $('#todo-list')
+      .append( todoItem ); 
+      $(".delete").on("click", function() {
+         $(this).parent().remove()
+      })  
+    }
+)
+
+
